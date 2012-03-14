b7793cc92aa8f1b55e513bc88319d727c2d7eb62
mason <mason@dreamerslab.com>
2012-03-14 13:42:49 +0800

    mongoose populate() usage

---
+++ javascript/nodejs/mongoose/populate.js
@@ -0,0 +1,58 @@
+// experimenting with mongoose's populate
+// http://mongoosejs.com/docs/populate.html
+
+var mongoose = require( "mongoose" );
+var Schema   = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+mongoose.connect("mongodb://localhost/populate_db");
+
+var UserSchema = new Schema({
+  name  : { type : String, required: true },
+  posts : [{ type : ObjectId, ref : 'Post' }]
+});
+
+var PostSchema = new Schema({
+  _user    : { type : ObjectId, ref : 'User' },
+  title    : { type : String, required : true },
+  content  : { type : String, required : true }
+});
+
+var User = mongoose.model( 'User', UserSchema );
+var Post = mongoose.model( 'Post', PostSchema );
+
+
+new User({
+  name : 'mason'
+})
+.save( function ( err, user ){
+  new Post({
+    _user   : user._id, // post ref to user
+    title   : 'title',
+    content : 'original content'
+  })
+  .save( function ( err, post ){
+
+    console.log( '\n# Both user & post are created!' );
+    console.log( '- user: ' + user );
+    console.log( '- post: ' + post );
+
+    user.posts.push( post );
+    user.save( function ( err, user ){
+      post.content = 'updated content'
+      post.save( function ( err, post ){
+
+        // Note that only finder methods can support populate()
+        User.find()
+        .populate( 'posts' )
+        .run( function( err, users ){
+          console.log( '\n# After updating the post' );
+          console.log( '- user : ' + users );
+          mongoose.disconnect();
+        });
+
+      });
+    });
+
+  });
+});
