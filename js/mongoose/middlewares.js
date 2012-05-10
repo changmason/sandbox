afd5155ec152a376ef8738a3a5b4cf83049f9198
mason <mason@dreamerslab.com>
2012-05-10 10:52:54 +0800

    experiment mongoose's middleware mechanisms

---
+++ js/mongoose/middlewares.js
@@ -0,0 +1,91 @@
+// experimenting with mongoose's middlewares
+// http://mongoosejs.com/docs/middleware.html
+
+var mongoose = require( "mongoose" );
+var Schema   = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+mongoose.connect("mongodb://localhost/middleware_db");
+
+var UserSchema = new Schema({
+  name  : { type : String, required: true },
+});
+
+UserSchema.pre( 'init', function ( next ){
+  console.log('HOOK: pre-init');
+  next();
+});
+
+UserSchema.post( 'init', function (){
+  console.log( 'HOOK: post-init' );
+});
+
+UserSchema.pre( 'save', function ( next ){
+  console.log('HOOK: pre-save');
+  next();
+});
+
+UserSchema.post( 'save', function (){
+  console.log( 'HOOK: post-save' );
+});
+
+// UserSchema.pre( 'isNew', function ( next ){
+//   console.log('HOOK: pre-isNew');
+//   next();
+// });
+
+// UserSchema.post( 'isNew', function (){
+//   console.log( 'HOOK: post-isNew' );
+// });
+
+UserSchema.pre( 'set', function ( next, path, val){
+  console.log( 'HOOK: pre-set ', path, '=', val );
+  next();
+});
+
+UserSchema.post( 'set', function ( path, val ){
+  console.log( 'HOOK: post-set ', path, '=', val );
+});
+
+UserSchema.pre( 'remove', function ( next ){
+  console.log('HOOK: pre-remove');
+  next();
+});
+
+UserSchema.post( 'remove', function (){
+  console.log( 'HOOK: post-remove' );
+});
+
+var User = mongoose.model( 'User', UserSchema );
+
+console.log( "= new User() =" );
+user = new User({ name : 'mason' });
+console.log( "= end of new =\n\n\n");
+
+console.log( "= user.save() =");
+user.save( function ( err, user ){
+  console.log( '= end of save() =\n\n\n' );
+
+  console.log( "= User.findOne() =");
+  User.findOne( function ( err, user ){
+    console.log( '= end of findOne() =\n\n\n' );
+
+    console.log( '= set user.name =' );
+    user.name = "chang";
+    console.log( '= end of set =\n\n\n' );
+
+    console.log( '= user.save() again =' );
+    user.save( function ( err, user ){
+      console.log( '= end of save() again =\n\n\n' );
+
+      console.log( '= user.remove() =' );
+      user.remove( function ( err, user ){
+        console.log( '= end of remove() =' );
+
+        mongoose.disconnect();
+      })
+    });
+  });
+});
+
+
