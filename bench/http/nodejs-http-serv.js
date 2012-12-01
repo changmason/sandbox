2347ac3880449d838e64c247ece81b878c683238
mason <mason@dreamerslab.com>
2012-12-01 13:39:14 +0800

    bind nodejs server to 0.0.0.0

--- bench/http/nodejs-http-serv.js
+++ bench/http/nodejs-http-serv.js
@@ -1,5 +1,6 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, "127.0.0.1");
+var http = require( 'http' );
+
+http.createServer( function ( req, res ){
+  res.writeHead( 200, { 'Content-Type' : 'text/plain' });
+  res.end( 'Hello World\n' );
+}).listen( 8080, "0.0.0.0" );
